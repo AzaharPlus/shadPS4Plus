36ac11241b2be7840cdc55e980d58726234dc108
Mahdi K. <106227973+m2k7m@users.noreply.github.com>
2025-07-14 16:22:03 +0300

    Fix Check Update

--- src/qt_gui/check_update.cpp
+++ src/qt_gui/check_update.cpp
@@ -45,10 +45,10 @@ void CheckUpdate::CheckForUpdates(const bool showMessage) {
     while (checkName) {
         updateChannel = QString::fromStdString(Config::getUpdateChannel());
         if (updateChannel == "Nightly") {
-            url = QUrl("https://api.github.com/repos/shadps4-emu/shadPS4/releases");
+            url = QUrl("https://api.github.com/repos/AzaharPlus/shadPS4Plus/releases");
             checkName = false;
         } else if (updateChannel == "Release") {
-            url = QUrl("https://api.github.com/repos/shadps4-emu/shadPS4/releases/latest");
+            url = QUrl("https://api.github.com/repos/AzaharPlus/shadPS4Plus/releases/latest");
             checkName = false;
         } else {
             if (Common::g_is_release) {
