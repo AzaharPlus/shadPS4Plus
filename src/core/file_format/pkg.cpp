848e8ebab3da5a109f177870ca5036c31e0d8473
ShadPs4Plus <shadps4@plus.ps4>
2026-01-02 10:25:45 +0100

    fix buffer sizes

--- src/core/file_format/pkg.cpp
+++ src/core/file_format/pkg.cpp
@@ -222,7 +222,11 @@ bool PKG::Extract(const std::filesystem::path& filepath, const std::filesystem::
         // Decrypt Np stuff and overwrite.
         if (entry.id == 0x400 || entry.id == 0x401 || entry.id == 0x402 ||
             entry.id == 0x403) { // somehow 0x401 is not decrypting
-            decNp.resize(entry.size);
+			int rsize = entry.size;
+			int msize = entry.size;
+			if(msize%16 != 0)
+				msize = msize - msize%16 + 16;
+            decNp.resize(msize);
             if (!file.Seek(entry.offset)) {
                 failreason = "Failed to seek to PKG entry offset";
                 return false;
@@ -232,13 +236,14 @@ bool PKG::Extract(const std::filesystem::path& filepath, const std::filesystem::
             data.resize(entry.size);
             file.ReadRaw<u8>(data.data(), entry.size);
 
-            std::span<u8> cipherNp(data.data(), entry.size);
+            std::span<u8> cipherNp(data.data(), msize);
             std::array<u8, 64> concatenated_ivkey_dk3_;
             std::memcpy(concatenated_ivkey_dk3_.data(), &entry, sizeof(entry));
             std::memcpy(concatenated_ivkey_dk3_.data() + sizeof(entry), dk3_.data(), sizeof(dk3_));
             PKG::crypto.ivKeyHASH256(concatenated_ivkey_dk3_, ivKey);
             PKG::crypto.aesCbcCfb128DecryptEntry(ivKey, cipherNp, decNp);
 
+            decNp.resize(rsize);
             Common::FS::IOFile out(extract_path / "sce_sys" / name,
                                    Common::FS::FileAccessMode::Write);
             out.Write(decNp);
