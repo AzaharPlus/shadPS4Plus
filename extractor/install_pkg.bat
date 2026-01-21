ebb4e23db5a5da7893bd88e07aede127d1069976
ShadPs4Plus <shadps4@plus.ps4>
2026-01-21 18:01:22 +0100

    batch install script windows

--- extractor/install_pkg.bat
+++ extractor/install_pkg.bat
@@ -1,18 +1,19 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+set par=%2%
 set gamesDir="PATH_TO_GAMES_DIR"
 set addonsDir="PATH_TO_ADDONS_DIR"
 
 IF %gamesDir%=="PATH_TO_GAMES_DIR" (
 	echo You need to update gamesDir with the games path used by ShadPs4
-	pause
+	IF NOT "%par%"=="--batch" pause
 	exit 0
 )
 
 IF %addonsDir%=="PATH_TO_ADDONS_DIR" (
 	echo You need to update addonsDir with the addons path used by ShadPs4
-	pause
+	IF NOT "%par%"=="--batch" pause
 	exit 0
 )
 
@@ -37,4 +38,4 @@ IF %errorlevel%==0 (
 	%0\..\pkg_extractor.exe %1 !gamesDir!
 )
 
-pause
+IF NOT "%par%"=="--batch" pause
